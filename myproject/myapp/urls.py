2edff0c3770e81a2f70ce698ccd42f09ae10f937
Harsh Jain <hjain24in@gmail.com>
2024-04-22 23:15:50 +0530

    commodity_country_selection

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('generate_video_country/<str:country>', views.running_bar_chart_country, name = 'generate_chart'),
     path('generate_video_home/', views.running_bar_chart_home, name = 'generate_video'),
     path('generate_video_home/<str:commodity>', views.running_bar_chart_home, name = 'generate_video'),
+    path('commodity_country_selection/', views.commodity_country_selection, name = 'commodity_country_selection'),
+    path('commodity_country_selection/<str:commodity>', views.commodity_country_selection, name = 'commodity_country_selection'),
 ]
